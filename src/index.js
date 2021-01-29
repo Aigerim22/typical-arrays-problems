263e88e795d053b276ab0f25b790db0c1b2afe30
Aigerim Tazhibay <aika0822t@gmail.com>
2021-01-30 01:03:14 +0600

    init: start

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
+exports.min = function min(array) {
+  if (!Array.isArray(array) || !array.length) {
+    return 0;
+  }
+  return Math.min(...array);
+};
+exports.max = function max(array) {
+  if (!Array.isArray(array) || !array.length) {
+    return 0;
+  }
+  return Math.max(...array);
+};
 
-exports.min = function min (array) {
-  return 0;
-}
-
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+  if (!Array.isArray(array) || !array.length) {
+    return 0;
+  }
+  let sum = 0;
+  for(let i =0; i<array.length; i++){
+  sum+=array[i];
+  }
+  return sum/array.length;
+};
